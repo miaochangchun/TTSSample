a967bb497e2ba3f70ba307c0534741ce2c0b181d
workStation <1004820109@qq.com>
2016-12-02 10:09:27 +0800

    Initial commit

---
+++ app/src/main/java/com/example/sinovoice/ttssample/MainActivity.java
@@ -0,0 +1,95 @@
+package com.example.sinovoice.ttssample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.example.sinovoice.util.HciCloudSysHelper;
+import com.example.sinovoice.util.HciCloudTtsHelper;
+import com.sinovoice.hcicloudsdk.common.HciErrorCode;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private EditText etText;
+    private Button btnPlay;
+    private Button btnPause;
+    private Button btnResume;
+    private Button btnStop;
+    private HciCloudSysHelper mHciCloudSysHelper;
+    private HciCloudTtsHelper mHciCloudTtsHelper;
+    private String initCapkeys = "tts.local.synth";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        initView();
+
+        initSinovoice();
+    }
+
+    @Override
+    protected void onDestroy() {
+        if (mHciCloudTtsHelper != null) {
+            mHciCloudTtsHelper.releaseTtsPlayer();
+        }
+        if (mHciCloudSysHelper != null) {
+            mHciCloudSysHelper.release();
+        }
+        super.onDestroy();
+    }
+
+    /**
+     * 灵云系统和tts功能初始化
+     */
+    private void initSinovoice() {
+        mHciCloudSysHelper = HciCloudSysHelper.getInstance();
+        mHciCloudTtsHelper = HciCloudTtsHelper.getInstance();
+        int errorCode = mHciCloudSysHelper.init(this);
+        if (errorCode != HciErrorCode.HCI_ERR_NONE) {
+            Toast.makeText(this, "系统初始化失败，错误码=" + errorCode, Toast.LENGTH_SHORT).show();
+            return;
+        }
+        mHciCloudTtsHelper.initTtsPlayer(this, initCapkeys);
+    }
+
+    /**
+     * 控件初始化
+     */
+    private void initView() {
+        etText = (EditText) findViewById(R.id.et_text);
+        btnPlay = (Button) findViewById(R.id.btn_play);
+        btnPause = (Button) findViewById(R.id.btn_pause);
+        btnResume = (Button) findViewById(R.id.btn_resume);
+        btnStop = (Button) findViewById(R.id.btn_stop);
+
+        btnPause.setOnClickListener(this);
+        btnPlay.setOnClickListener(this);
+        btnResume.setOnClickListener(this);
+        btnStop.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_pause:
+                mHciCloudTtsHelper.pauseTtsPlayer();
+                break;
+            case R.id.btn_play:
+                mHciCloudTtsHelper.playTtsPlayer(etText.getText().toString(), "tts.local.synth");
+                break;
+            case R.id.btn_resume:
+                mHciCloudTtsHelper.resumeTtsPlayer();
+                break;
+            case R.id.btn_stop:
+                mHciCloudTtsHelper.stopTtsPlayer();
+                break;
+            default:
+                break;
+        }
+    }
+}
---
+++ app/src/main/java/com/example/sinovoice/util/HciCloudTtsHelper.java
@@ -0,0 +1,165 @@
+package com.example.sinovoice.util;
+
+import android.content.Context;
+import android.util.Log;
+
+import com.sinovoice.hcicloudsdk.android.tts.player.TTSPlayer;
+import com.sinovoice.hcicloudsdk.common.tts.TtsConfig;
+import com.sinovoice.hcicloudsdk.common.tts.TtsInitParam;
+import com.sinovoice.hcicloudsdk.player.TTSCommonPlayer;
+import com.sinovoice.hcicloudsdk.player.TTSPlayerListener;
+
+/**
+ * Created by miaochangchun on 2016/11/28.
+ */
+public class HciCloudTtsHelper {
+    private static final String TAG = HciCloudTtsHelper.class.getSimpleName();
+    private static HciCloudTtsHelper mHciCloudTtsHelper = null;
+    private TTSPlayer mTtsPlayer;
+
+    private HciCloudTtsHelper(){
+    }
+
+    /**
+     * 获取初始化对象
+     * @return
+     */
+    public static HciCloudTtsHelper getInstance() {
+        if (mHciCloudTtsHelper == null) {
+            return  new HciCloudTtsHelper();
+        }
+        return mHciCloudTtsHelper;
+    }
+
+    /**
+     * 播放器初始化
+     * @param context   上下文
+     * @param initCapkeys   需要初始化的capkey，可以设置为多个，中间以；间隔
+     */
+    public void initTtsPlayer(Context context, String initCapkeys){
+        mTtsPlayer = new TTSPlayer();
+        String strConfig = getTtsInitParam(context, initCapkeys);
+        mTtsPlayer.init(strConfig, new TTSEventProcess());
+        //设置使用AudioFocus机制
+        mTtsPlayer.setContext(context);
+        Log.d(TAG, "initTtsPlayer Success.");
+    }
+
+    /**
+     * 开始播放
+     * @param text  需要播放的文本
+     * @param capkey    发音人选择
+     */
+    public void playTtsPlayer(String text, String capkey) {
+        String strConfig = getTtsSynthConfig(capkey);
+
+        Log.d(TAG, "mTtsPlayer = " + mTtsPlayer);
+        if (mTtsPlayer.getPlayerState() == TTSCommonPlayer.PLAYER_STATE_PLAYING || mTtsPlayer.getPlayerState() == TTSCommonPlayer.PLAYER_STATE_PAUSE) {
+            mTtsPlayer.stop();
+        }
+        if (mTtsPlayer.getPlayerState() == TTSCommonPlayer.PLAYER_STATE_IDLE) {
+            mTtsPlayer.play(text, strConfig);
+        }
+    }
+
+    /**
+     * 暂停播放
+     */
+    public void pauseTtsPlayer(){
+        if (mTtsPlayer.getPlayerState() == TTSCommonPlayer.PLAYER_STATE_PLAYING) {
+            mTtsPlayer.pause();
+        }
+    }
+
+    /**
+     * 恢复播放
+     */
+    public void resumeTtsPlayer(){
+        if (mTtsPlayer.getPlayerState() == TTSCommonPlayer.PLAYER_STATE_PAUSE) {
+            mTtsPlayer.resume();
+        }
+    }
+
+    /**
+     * 停止播放
+     */
+    public void stopTtsPlayer(){
+        if (mTtsPlayer.canStop()){
+            mTtsPlayer.stop();
+        }
+    }
+
+    /**
+     * 公有云配置参数
+     * @param capkey
+     * @return
+     */
+    private String getTtsSynthConfig(String capkey) {
+        TtsConfig ttsConfig = new TtsConfig();
+        ttsConfig.addParam(TtsConfig.SessionConfig.PARAM_KEY_CAP_KEY, capkey);
+        ttsConfig.addParam(TtsConfig.BasicConfig.PARAM_KEY_AUDIO_FORMAT, "pcm16k16bit");
+        ttsConfig.addParam(TtsConfig.BasicConfig.PARAM_KEY_SPEED, "5");
+        ttsConfig.addParam(TtsConfig.EncodeConfig.PARAM_KEY_ENCODE, "speex");
+        return  ttsConfig.getStringConfig();
+    }
+
+    /**
+     * 获取TTS的参数配置
+     * @param context
+     * @param capkey    发音人
+     * @return
+     */
+    private String getTtsInitParam(Context context, String capkey) {
+        TtsInitParam ttsInitParam = new TtsInitParam();
+        String dataPath = context.getFilesDir().getAbsolutePath().replace("files", "lib");
+        ttsInitParam.addParam(TtsInitParam.PARAM_KEY_DATA_PATH, dataPath);
+        ttsInitParam.addParam(TtsInitParam.PARAM_KEY_FILE_FLAG, "android_so");
+        ttsInitParam.addParam(TtsInitParam.PARAM_KEY_INIT_CAP_KEYS, capkey);
+        return ttsInitParam.getStringConfig();
+    }
+
+    /**
+     * 播放器反初始化
+     */
+    public void releaseTtsPlayer(){
+        if (mTtsPlayer != null) {
+            mTtsPlayer.release();
+        }
+    }
+
+    /**
+     * 播放器回调类
+     */
+    private class TTSEventProcess implements TTSPlayerListener {
+
+        /**
+         * 播放器的状态回调
+         * @param playerEvent
+         */
+        @Override
+        public void onPlayerEventStateChange(TTSCommonPlayer.PlayerEvent playerEvent) {
+
+        }
+
+        /**
+         * 播放器播放进度回调
+         * @param playerEvent
+         * @param i
+         * @param i1
+         */
+        @Override
+        public void onPlayerEventProgressChange(TTSCommonPlayer.PlayerEvent playerEvent, int i, int i1) {
+
+        }
+
+        /**
+         * 播放器的错误回调
+         * @param playerEvent
+         * @param i
+         */
+        @Override
+        public void onPlayerEventPlayerError(TTSCommonPlayer.PlayerEvent playerEvent, int i) {
+
+        }
+    }
+}
